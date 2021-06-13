611acc0df0b1cb36cff97dc191109ec4391b5209
ARTrex007 <arturmarksz7@gmail.com>
2021-06-13 21:37:15 +0200

    7.1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,6 +7,8 @@ public class Main
     {
         Human human1 = new Human("Stefan", "Czarnecki", "Lekarz", 28, 73 );
         Human human2 = new Human("Stefan", "Czarnecki", "Lekarz", 28, 73 );
+        Animal animal1 = new Animal("Ryszard", "Kot", false, 4);
+        Phone phone1 = new Phone("Redmi", "Xiaomi", 0.5);
         Car car1 = new Car("Mustang", "Ford", "GD008FB", 2, 5000);
 
         car1.Czy_Marka(Car.marki.Ford);
@@ -15,5 +17,8 @@ public class Main
         System.out.println(human2.hashCode());
         System.out.println(human1);
         System.out.println(human2);
+        System.out.println(animal1);
+        System.out.println(phone1);
+        System.out.println(car1);
     }
 }
